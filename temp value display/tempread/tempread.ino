4ac054dea24b3752d3b273fea0e9ba7e9c8076ce
fasilkv <fasilkv000@gmail.com>
2019-11-06 14:59:47 +0530

    aligned code

--- temp value display/tempread/tempread.ino
+++ temp value display/tempread/tempread.ino
@@ -1,12 +1,11 @@
 // Bluetooth temperature sensor
+
 #include "DHT.h"
 
+//#define DHTPIN 3    
 #define DHTPIN 3
-
-// Pin for the DHT sensor
-//#define DHTPIN 7    
 //#define DHTTYPE DHT22
- #define DHTTYPE DHT11
+#define DHTTYPE DHT11
 
 // Create instance for the DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
@@ -16,18 +15,13 @@ void setup(void)
 {
   dht.begin();
   Serial.begin(115200);
+  // for usb serial communication
   //Serial.println(F("DHTxx test!"));
-
 }
-
-
 
 void loop(void)
 {
-  // Wait a few seconds between measurements.
-  //delay(2000);
-
-  // Read temperature as Celsius (the default)
+ 
   //float t = dht.readTemperature();
   
     // Get command
@@ -40,7 +34,8 @@ void loop(void)
       
       // If a measurement is required, measure data and send it back
       if (c == 'm'){
-        
+           
+          // Read temperature and humidity (the default)
           float h = dht.readHumidity();
           float t = dht.readTemperature();
           
